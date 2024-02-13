6ebe59d4aea9660ad1f909920136f44f5c82300c
hayaty88 <payagram01@gmail.com>
2024-02-13 23:43:52 +1100

    Install React and setup, and fetch API

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import "./App.css";
+import { getAllPokemon } from "./utils/pokemon.js";
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // All Pokemon Data
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
+  return <div className="App"></div>;
+}
+
+export default App;
