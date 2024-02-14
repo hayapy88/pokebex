ce187fe96252aa50918452603072f9b41bf22235
hayaty88 <payagram01@gmail.com>
2024-02-15 02:20:37 +1100

    Install Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ function App() {
     };
     fetchPokemonData();
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
