923aaaa20bee7100511339f3adb149fd3639dff2
hayaty88 <payagram01@gmail.com>
2024-02-15 03:35:31 +1100

    Get Pokemon detailed data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon.js";
+import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
   useEffect(() => {
     const fetchPokemonData = async () => {
       // All Pokemon Data
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // Get Pokemon data in detail
+      loadPokemon(res.results);
+      // console.log(res.results);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {loading ? <h1>Now Loading...</h1> : <h1>Recieved data successfully!</h1>}
     </div>
   );
 }
