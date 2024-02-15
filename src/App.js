63e5bef467c15c88f3642e14cdb0b882ee8a4e7a
hayaty88 <payagram01@gmail.com>
2024-02-16 07:59:36 +1100

    Create next page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 import Card from "./components/Card/Card.js";
+import Navbar from "./components/Navbar/Navbar.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
   useEffect(() => {
     const fetchPokemonData = async () => {
       // All Pokemon Data
       let res = await getAllPokemon(initialURL);
       // Get Pokemon data in detail
       loadPokemon(res.results);
-      // console.log(res.results);
+      // console.log(res.next);
+      setNextURL(res.next);
       setLoading(false);
     };
     fetchPokemonData();
@@ -21,7 +24,7 @@ function App() {
   const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
       data.map((pokemon) => {
-        console.log(pokemon);
+        // console.log(pokemon);
         let pokemonRecord = getPokemon(pokemon.url);
         return pokemonRecord;
       })
@@ -29,22 +32,38 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
-  console.log(pokemonData);
+  // console.log(pokemonData);
+
+  const nextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    console.log(data);
+    loadPokemon(data.results);
+    setLoading(false);
+  };
+  const prevPage = () => {};
 
   return (
-    <div className="App m-1 sm:m-2">
-      {loading ? (
-        <h1>Now Loading...</h1>
-      ) : (
-        <>
-          <div className="pokemonCardContainer grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 sm:gap-x-0 gap-y-4 my-4">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App mx-1 mt-16 mb-10">
+        {loading ? (
+          <h1 className="mt-20">Now Loading...</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 sm:gap-x-0 gap-y-4 my-4">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="pager">
+              <button onClick={prevPage}>Prev</button>
+              <button onClick={nextPage}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed w-full top-0 h-12 bg-sky-600 text-white text-xl flex justify-center items-center">
+      Pokebox
+    </nav>
+  );
+};
+
+export default Navbar;
