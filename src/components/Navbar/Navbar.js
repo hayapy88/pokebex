fba7b04f03021f85d8ef813f9f13f562712c1f68
hayaty88 <payagram01@gmail.com>
2024-02-21 23:32:21 +1100

    Change app name

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="fixed w-full top-0 h-12 bg-sky-600 text-white text-xl flex justify-center items-center">
-      Pokebox
+      PokeBox
     </nav>
   );
 };
