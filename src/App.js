e0c30cd7238f8933e14e26cf715bc5040a4a7484
hayaty88 <payagram01@gmail.com>
2024-03-15 23:59:22 +1100

    Remove old code and files not being used

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 import Card from "./components/Card/Card.js";
 import Navbar from "./components/Navbar/Navbar.js";
 import Loading from "./components/Loading/Loading.js";
-// import Loadmore from "./components/Pagination/Loadmore.js";
 import Search from "./components/Search/Search.js";
 import InfiniteScroll from "./components/Pagination/InfiniteScroll.js";
-// import Pagination from "./components/Pagination/Pagination.js";
-// import Search from "./components/Search/Search.js";
 
 function App() {
   const totalPokemon = 493;
@@ -35,20 +32,14 @@ function App() {
   ];
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
-  // const [visible, setVisible] = useState(30);
   const [query, setQuery] = useState("");
   const [activeType, setActiveType] = useState(pokemonTypes);
-  // const [nextURL, setNextURL] = useState("");
-  // const [prevURL, setPrevURL] = useState("");
   useEffect(() => {
     const fetchPokemonData = async () => {
       // All Pokemon Data
       let res = await getAllPokemon(initialURL);
       // Get Pokemon data in detail
       loadPokemon(res.results);
-      // console.log(res.next);
-      // setNextURL(res.next);
-      // setPrevURL(res.previous); // Null
       setTimeout(() => {
         setLoading(false);
       }, 2000);
@@ -96,33 +87,6 @@ function App() {
 
   // console.log(pokemonData);
 
-  // const handleNextPage = async () => {
-  //   setLoading(true);
-  //   let data = await getAllPokemon(nextURL);
-  //   console.log(data);
-  //   await loadPokemon(data.results);
-  //   setNextURL(data.next);
-  //   setPrevURL(data.previous);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 800);
-  // };
-  // const handlePrevPage = async () => {
-  //   if (!prevURL) return;
-  //   setLoading(true);
-  //   let data = await getAllPokemon(prevURL);
-  //   console.log(data);
-  //   await loadPokemon(data.results);
-  //   setNextURL(data.next);
-  //   setPrevURL(data.previous);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000);
-  // };
-
-  // const showMorePokemon = () => {
-  //   setVisible((prevValue) => prevValue + 30);
-  // };
   return (
     <>
       {loading ? (
@@ -139,10 +103,6 @@ function App() {
                 pokemonTypes={pokemonTypes}
                 handleAllTypes={handleAllTypes}
               />
-              {/* <div className="pokemonCardContainer grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 sm:gap-x-0 gap-y-4 sm:mt-14 pt-6 mb-4">
-                {filteredPokemonData.slice(0, visible).map((pokemon, i) => {
-                  return <Card key={i} pokemon={pokemon} />;
-                })} */}
               <div className="pokemonCardContainer grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 sm:gap-x-0 gap-y-4 sm:mt-14 pt-6 mb-4">
                 {filteredPokemonData.map((pokemon, i) => {
                   return <Card key={i} pokemon={pokemon} />;
@@ -150,16 +110,6 @@ function App() {
               </div>
 
               <InfiniteScroll />
-
-              {/* <Loadmore
-                showMorePokemon={showMorePokemon}
-                visible={visible}
-                totalPokemon={totalPokemon}
-              /> */}
-              {/* <Pagination
-                handlePrevPage={handlePrevPage}
-                handleNextPage={handleNextPage}
-              /> */}
             </div>
           </div>
         </div>
