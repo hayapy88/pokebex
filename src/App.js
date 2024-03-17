9b6a9220b2a079362fe18c33e4a5e6a59cce72a6
hayaty88 <payagram01@gmail.com>
2024-03-17 23:58:47 +1100

    Add Load more button to load next 30 pokemons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
+import { getPokemon } from "./utils/pokemon.js";
 import Card from "./components/Card/Card.js";
 import Navbar from "./components/Navbar/Navbar.js";
 import Loading from "./components/Loading/Loading.js";
@@ -8,8 +8,9 @@ import Search from "./components/Search/Search.js";
 import InfiniteScroll from "./components/Pagination/InfiniteScroll.js";
 
 function App() {
-  const totalPokemon = 493;
-  const initialURL = "https://pokeapi.co/api/v2/pokemon?limit=" + totalPokemon;
+  // const totalPokemon = 493;
+  const [page, setPage] = useState(1);
+
   const pokemonTypes = [
     "bug",
     "dark",
@@ -34,34 +35,51 @@ function App() {
   const [pokemonData, setPokemonData] = useState([]);
   const [query, setQuery] = useState("");
   const [activeType, setActiveType] = useState(pokemonTypes);
+
   useEffect(() => {
     const fetchPokemonData = async () => {
-      // All Pokemon Data
-      let res = await getAllPokemon(initialURL);
-      // Get Pokemon data in detail
-      loadPokemon(res.results);
-      setTimeout(() => {
-        setLoading(false);
-      }, 2000);
+      // Update Pokemon URL
+      const fetchPokemonURL = `https://pokeapi.co/api/v2/pokemon?limit=30&offset=${
+        30 * (page - 1)
+      }`;
+
+      // Get Pokemon name and URL from limited fetchPokemonURL.
+      // eg) [{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" }, {}, {}, ...]
+      let res = await getPokemon(fetchPokemonURL);
+      console.log("res");
+      console.log(res);
+
+      // Get each Pokemon data
+      const getEachPokemonData = async (data) => {
+        let _pokemonData = await Promise.all(
+          data.map((pokemon) => {
+            console.log(pokemon);
+            let pokemonRecord = getPokemon(pokemon.url);
+            return pokemonRecord;
+          })
+        );
+        setPokemonData((prevPokemonData) => [
+          ...prevPokemonData,
+          ..._pokemonData,
+        ]);
+        console.log("_pokemonData");
+        console.log(_pokemonData);
+      };
+
+      getEachPokemonData(res.results);
+      console.log(res.results);
+      setLoading(false);
     };
     fetchPokemonData();
-  }, [initialURL]);
-  const loadPokemon = async (data) => {
-    let _pokemonData = await Promise.all(
-      data.map((pokemon) => {
-        // console.log(pokemon);
-        let pokemonRecord = getPokemon(pokemon.url);
-        return pokemonRecord;
-      })
-    );
-    setPokemonData(_pokemonData);
-  };
-  const filteredPokemonData = pokemonData.filter((pokemon) => {
+  }, [page]);
+
+  const displayablePokemonArray = pokemonData.filter((pokemon) => {
     return (
       pokemon.name.toLowerCase().includes(query.toLowerCase()) &&
       pokemon.types.some((type) => activeType.includes(type.type.name))
     );
   });
+
   const handleInputChange = (newQuery) => {
     setQuery(newQuery);
   };
@@ -83,7 +101,7 @@ function App() {
       }
     });
   };
-  console.log(activeType);
+  // console.log(activeType);
 
   // console.log(pokemonData);
 
@@ -104,11 +122,11 @@ function App() {
                 handleAllTypes={handleAllTypes}
               />
               <div className="pokemonCardContainer grid sm:grid-cols-2 md:grid-cols-3 gap-x-8 sm:gap-x-0 gap-y-4 sm:mt-14 pt-6 mb-4">
-                {filteredPokemonData.map((pokemon, i) => {
+                {displayablePokemonArray.map((pokemon, i) => {
                   return <Card key={i} pokemon={pokemon} />;
                 })}
               </div>
-
+              <button onClick={() => setPage((prev) => prev + 1)}>Load</button>
               <InfiniteScroll />
             </div>
           </div>
