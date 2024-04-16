507e77633404077fb2b81eea47c44e1d94fd040b
hayaty88 <payagram01@gmail.com>
2024-04-17 01:31:36 +1000

    Adjust title font-size

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 const Navbar = ({ t, i18n }) => {
   return (
-    <nav className="fixed w-full top-0 h-12 bg-sky-600 text-white text-xl flex justify-center items-center">
+    <nav className="fixed w-full top-0 h-12 bg-sky-600 text-white text-lg sm:text-xl flex justify-center items-center">
       {t("title")}
       <div className="absolute right-2 text-sm">
         <button onClick={() => i18n.changeLanguage("en")} className="mr-2">
